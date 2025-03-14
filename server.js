a361dc8aed61b37d4f68356ba4f3ace1b4e71a4c
SaifD227 <saifaalii237@gmail.com>
2025-03-14 11:06:30 +0500

    cors frontend  allow api

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000", "https://your-frontend.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true 
+  }));
+  
 app.use(bodyParser.json());
 
 
